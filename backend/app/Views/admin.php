aebcf67d332fbba0912a6bd531d7c8d3a6735580
ismailaydin52 <ismailaydin52002@gmail.com>
2024-03-24 18:54:39 +0300

    Update admin.php

--- backend/app/Views/admin.php
+++ backend/app/Views/admin.php
@@ -299,10 +299,10 @@
     var myChart = new Chart(ctx, {
         type: 'line',
         data: {
-            labels: ['Ocak', 'Şubat', 'Mart', 'Nisan', 'Mayıs', 'Haziran'],
+            labels: ['zonguldak-izmit', 'izmir-zonguldak', 'izmit-istanbul', 'izmit-izmir', 'istanbul-zonguldak', 'istanbul izmit'],
             datasets: [{
                 label: 'Satışlar',
-                data: [12, 19, 3, 5, 2, 3],
+                data: [10, 5, 20, 7, 2, 16],
                 backgroundColor: 'rgba(255, 99, 132, 0.2)',
                 borderColor: 'rgba(255, 99, 132, 1)',
                 borderWidth: 1
